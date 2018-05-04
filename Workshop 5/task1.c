dcd7c09ab5bf9b27a4fb9828246bb99306f1c8d4
theghia <theghiactr@gmail.com>
2018-05-04 15:26:07 +0100

    Add files via upload

--- Workshop 5/task1.c
+++ Workshop 5/task1.c
@@ -6,34 +6,34 @@ void sig_handler(int);
 
 int main(void)
 {
-	/* If SIGUSR1 is received, then call the function sig_handler */
-	if(signal(SIGUSR1, sig_handler) == SIG_ERR)
-	{
-		printf("\nCan't catch SIGUSR1\n");
-	}
+    /* If SIGUSR1 is received, then call the function sig_handler */
+    if(signal(SIGUSR1, sig_handler) == SIG_ERR)
+    {
+	printf("\nCan't catch SIGUSR1\n");
+    }
 	
-	/* A long long wait so that we can easily issue a signal to this process */
-	while(1)
-	{
-		/* Does not use as much CPU time as an infinite while loop */
-		sleep(1);
-	}
-	printf("Finished\n");
+    /* A long long wait so that we can easily issue a signal to this process */
+    while(1)
+    {
+	/* Does not use as much CPU time as an infinite while loop */
+	sleep(1);
+    }
+    printf("Finished\n");
 	
-	return 0;
+    return 0;
 }
 
 void sig_handler(int signo)
 {
-	if (signo == SIGUSR1)
-	{
-		printf("Received SIGUSR1\n");
-		exit(0);
+    if (signo == SIGUSR1)
+    {
+	printf("Received SIGUSR1\n");
+	exit(0);
 		
-		/* If SIGUSR1 is received, then call the function sig_handler */
-		if(signal(SIGUSR1, sig_handler) == SIG_ERR)
-		{
-			printf("\nCan't catch SIGUSR1\n");
-		}
+	/* If SIGUSR1 is received, then call the function sig_handler */
+	if(signal(SIGUSR1, sig_handler) == SIG_ERR)
+	{
+	    printf("\nCan't catch SIGUSR1\n");
 	}
+    }
 }
--- Workshop 5/task2.c
+++ Workshop 5/task2.c
@@ -2,19 +2,64 @@
 #include <stdlib.h>
 #include <signal.h>
 
+void sig_handler (int);
+void sig2_handler(int);
+
 int main(void)
 {
-	int i;
-	int x;
+    int i;
+    int x;
+    
+    /* If SIGUSR1 is received, then call the function sig_handler */
+    if(signal(SIGUSR1, sig_handler) == SIG_ERR)
+    {
+	printf("\nCan't catch SIGUSR1\n");
+    }
 	
-	
-	for (i = -5; i <= 5; i++)
+    sleep(20);
+    
+    if(signal(SIGFPE, sig2_handler) == SIG_ERR)
+    {
+        printf("\nCan't catch SIGFPE\n");
+    }
+    
+    for (i = -5; i <= 5; i++)
+    {
+        x = 10/i;
+	printf("%d: x = %d\n", i,x);
+    }
+    
+    printf("Finished\n");
+
+    return 0;
+}
+
+void sig_handler(int signo)
+{
+    if (signo == SIGUSR1)
+    {
+	printf("Received SIGUSR1\n");
+	/*exit(0);*/
+		
+	/* If SIGUSR1 is received, then call the function sig_handler */
+	if(signal(SIGUSR1, sig_handler) == SIG_ERR)
 	{
-		x = 10/i;
-		printf("%d: x = %d\n", i, x);
+	    printf("\nCan't catch SIGUSR1\n");
 	}
-	
-	printf("Finished\n");
-	
-	return 0;
+    }
+}
+
+void sig2_handler(int signo)
+{
+   if (signo == SIGFPE)
+    {
+	printf("I have detected an aithmetic error and am giving up\n");
+	exit(0);
+		
+	/* If SIGFPE is received, then call the function sig2_handler */
+	if(signal(SIGFPE, sig_handler) == SIG_ERR)
+	{
+	    printf("\nCan't catch SIGFPE\n");
+	}
+    }
 }
